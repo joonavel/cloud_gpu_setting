d03094777069b64a7bee38f97c046c1e71a1f533
joonavel <joonavel@gmail.com>
2024-11-23 02:21:33 +0900

    update cuda_install.sh

--- cuda_install.sh
+++ cuda_install.sh
@@ -2,7 +2,8 @@
 
 # CUDA 및 NVIDIA 드라이버 설치
 sudo apt-get update && echo "System update complete."
-sudo apt install -y nvidia-driver && echo "NVIDIA driver installed."
+sudo apt install -y ubuntu-drivers-common
+sudo ubuntu-drivers autoinstall && echo "NVIDIA driver installed."
 
 # 시스템 재부팅 (주의: 스크립트 재실행 필요)
 sudo reboot
